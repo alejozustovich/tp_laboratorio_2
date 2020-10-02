80f8af30b36b45c138f3b767c212a3063ca0e431
alejozustovich <alejozustovich@gmail.com>
2020-10-01 23:16:32 -0300

    Trabajo Practico 2

---
+++ Trabajo Practico 2/Entidades/Suv.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Suv : Vehiculo
+    {
+        #region CONSTRUCTOR
+        /// <summary>
+        /// Constructor parametrizado recibe valores
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Suv(EMarca marca, string chasis, ConsoleColor color) : base(chasis, marca, color)
+        {
+        }
+        #endregion
+
+        #region PROPIEDADES
+        /// <summary>
+        /// Las camionetas son grandes
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+        #endregion
+
+        #region METODOS
+        /// <summary>
+        /// Sobreescribe el método Mostrar del Vehículo con los atributos de Suv
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SUV");
+            sb.AppendLine((string)this);
+            sb.AppendLine("TAMAÑO : " + this.Tamanio);
+            sb.AppendLine("---------------------");
+            sb.AppendLine("");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
---
+++ Trabajo Practico 2/Entidades/Vehiculo.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// La clase Vehiculo no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Vehiculo
+    {
+        #region ENUMERADOS
+        public enum EMarca
+        {
+            Chevrolet, Ford, Renault, Toyota, BMW, Honda, HarleyDavidson
+        }
+        public enum ETamanio
+        {
+            Chico, Mediano, Grande
+        }
+        #endregion
+
+        #region ATRIBUTOS
+        protected ETamanio tamanio;
+        protected EMarca marca;  
+        protected string chasis; 
+        protected ConsoleColor color; 
+        #endregion
+
+        #region PROPIEDADES
+        /// <summary>
+        /// ReadOnly: Retornará el tamaño
+        /// </summary>
+        protected virtual ETamanio Tamanio 
+        { 
+            get 
+            { 
+                return this.tamanio; 
+            }
+        }
+        #endregion
+
+        #region CONSTRUCTOR
+        /// <summary>
+        /// Constructor parametrizado de un Vehículo
+        /// </summary>
+        /// <param name="chasis"></param>
+        /// <param name="marca"></param>
+        /// <param name="color"></param>
+        protected Vehiculo(string chasis, EMarca marca, ConsoleColor color)
+        {
+            this.chasis = chasis;
+            this.marca = marca;
+            this.color = color;
+        }
+        #endregion
+
+        #region METODOS
+        /// <summary>
+        /// Publica todos los datos del Vehiculo.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            return this.ToString();
+        }
+        #endregion
+
+        #region SOBRECARGAS
+        /// <summary>
+        /// Muestra todos los atributos
+        /// </summary>
+        /// <param name="p"></param>
+        public static explicit operator string(Vehiculo p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CHASIS: " + p.chasis);
+            sb.AppendLine("MARCA : " + p.marca.ToString());
+            sb.AppendLine("COLOR : " + p.color.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Dos vehiculos son iguales si comparten el mismo chasis
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Vehiculo v1, Vehiculo v2)
+        {
+            bool sonIguales = false;
+
+            if ( !(v1 is null) && !(v2 is null) && (v1.chasis == v2.chasis))
+            {
+                sonIguales = true;
+            }
+
+            return sonIguales;
+        }
+
+        /// <summary>
+        /// Dos vehiculos son distintos si su chasis es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            return !(v1 == v2);
+        }
+        #endregion
+    }
+}
